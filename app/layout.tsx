3bf759c6182065ccb972f45773d84ad14499d4d4
dimm47 <ddimo017@gmail.com>
2024-07-29 11:09:45 +0100

    push to production

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,5 @@
 /* eslint-disable camelcase */
-import {
-  ClerkProvider
-  // SignInButton,
-  // SignedIn,
-  // SignedOut,
-  // UserButton
-} from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
 import { Inter, Space_Grotesk } from 'next/font/google';
 import type { Metadata } from 'next';
 
