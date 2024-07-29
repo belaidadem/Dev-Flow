f58f3739f4f1849b2d72cb57f6c079fbe1891a23
dimm47 <ddimo017@gmail.com>
2024-07-29 23:37:37 +0100

    debugging webhook error

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -11,6 +11,7 @@ import {
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
+  console.log('Received request:', req);
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
