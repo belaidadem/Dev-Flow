950f359d939c0f713a3562486d67e48f4524af7a
dimm47 <ddimo017@gmail.com>
2024-07-29 14:04:29 +0100

    debugging webhook error

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,14 +1,12 @@
 import Question from '@/components/forms/Question';
 import { getUserById } from '@/lib/actions/user.action';
-// import { auth } from '@clerk/nextjs/server';
+import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
 import React from 'react';
 
 const Page = async () => {
-  // const { userId } = auth();
-
-  const userId = '12345';
+  const { userId } = auth();
 
   if (!userId) redirect('/sign-in');
 
@@ -20,9 +18,9 @@ const Page = async () => {
         Ask a question
       </h1>
       <div className='mt-9'>
-        <Question
+        {/* <Question
           mongoUserId={JSON.stringify(mongoUser._id)}
-        />
+        /> */}
       </div>
     </div>
   );
