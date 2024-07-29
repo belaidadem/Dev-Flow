42866f1ebe15ec0c790f6307b57f19c34609eef5
dimm47 <ddimo017@gmail.com>
2024-07-29 13:21:38 +0100

    debugging webhook error

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -112,6 +112,8 @@ export async function POST(req: Request) {
       path: `/profile/${id}`
     });
 
+    console.log('user.updated');
+
     return NextResponse.json({
       message: 'OK',
       user: monogoUser
