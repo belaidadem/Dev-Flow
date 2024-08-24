53428d0c6894b2fe9235f56d06e61e3ae4d382b6
dimm47 <ddimo017@gmail.com>
2024-08-24 03:20:07 +0100

    adding upcomming soon jobs feature

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className='body-regular text-dark100_light900 font-semibold'>
+      Upcomming Soon...
+    </div>
+  );
+};
+
+export default Page;
